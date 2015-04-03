f38340be4062c93ae3e7620de5e07df99bd91a92
Hugh <hugh_77@icloud.com>
2015-04-04 00:03:31 +0800

    finish top navigation

---
+++ js/share_content.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Hugh on 2015/4/3 0003.
+ */
+function add_header(){
+    var str_header = "<header>"+
+        "<div id='nav_header'>"+
+        "<h1><a href='/index.html'></a></h1>"+
+        "<div id='header_right'>"+
+        "<ul>"+
+        "<li class='vertical_border'></li>"+
+        "<li><a href='/login.html'>登陆</a></li>"+
+        "<li class='vertical_border'></li>"+
+        "<li><a href='/signup.html'>注册</a></li>"+
+        "<li class='vertical_border'></li>"+
+        "</ul>"+
+        "</div>"+
+        "</div>"+
+        "</header>";
+    document.write(str_header);
+}
+
