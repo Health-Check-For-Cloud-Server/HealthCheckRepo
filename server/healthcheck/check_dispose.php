8b488815f17fba65a94e63e6ae712a41bbc2b770
Hugh <hugh_77@icloud.com>
2015-06-04 18:46:50 +0800

    complete server side call

--- server/healthcheck/check_dispose.php
+++ server/healthcheck/check_dispose.php
@@ -6,7 +6,18 @@
  * Time: 9:18
  */
     if($mid_code = $_GET['mid_code']) {
-        echo $mid_code;
-    }
+        $file_name = "./json_files/".time().".json";
+        $json_file = fopen($file_name,"w");
+        fwrite($json_file,$mid_code);
+        fclose($json_file);
 
-?>
+        $result = array();
+        exec('python ./modules/check.py '.$file_name, $result);
+        unlink($file_name);
+
+        $result_str = "";
+        foreach ($result as $x)
+            $result_str += $x;
+
+        echo $result_str;
+    }
